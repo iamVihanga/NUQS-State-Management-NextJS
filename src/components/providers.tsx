d502d56ce6010114e72cac2aa0daad7634865288
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2025-02-04 21:38:06 +0530

    Add: Initialize ShadCN & Nuqs

---
+++ src/components/providers.tsx
@@ -0,0 +1,10 @@
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+
+export function Providers({ children }:{ children: React.ReactNode }) {
+  return (
+    <NuqsAdapter>
+        {children}
+    </NuqsAdapter>
+  )
+    
+}
